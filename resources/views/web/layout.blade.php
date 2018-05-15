c2db0e6ddf584df5b0f2a58c04e50c575af654fb
SilentLittleCat <13438082119@163.com>
2018-05-15 20:24:56 +0800

    test

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Silent Show dd</title>
+        <title>Silent Show aa</title>
 
         <!-- Fonts -->
         {{--<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">--}}

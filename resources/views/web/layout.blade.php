433307317bcdfe8610d23534dc5e4c4d44271eda
SilentLittleCat <13438082119@163.com>
2018-05-15 19:14:33 +0800

    test

--- resources/views/web/layout.blade.php
+++ resources/views/web/layout.blade.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Silent Show bb</title>
+        <title>Silent Show bbff</title>
 
         <!-- Fonts -->
         {{--<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">--}}

691fdf814af9ef7c0a71d5935e684d759eb993ab
SilentLittleCat <13438082119@163.com>
2018-05-16 16:42:50 +0800

    test

--- resources/views/friends/learn/fun/create.blade.php
+++ resources/views/friends/learn/fun/create.blade.php
@@ -38,7 +38,7 @@
                 </div>
 
                 <div class="form-group {{ $errors->has('image') ? 'has-error' : '' }}">
-                    <label class="col-sm-2 col-sm-offset-2 control-label">图片(600*300)</label>
+                    <label class="col-sm-2 col-sm-offset-2 control-label">图片(600*400)</label>
                     <div class="col-sm-6">
                         @include('components.upload-images', ['name' => 'image', 'value' => (isset(old('data')['image']) ? old('data')['image'] : ''), 'type' =>'single', 'class' => 'learn-fun'])
                     </div>

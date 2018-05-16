468ac20728863419e1459b82e0b8c278e8d6535a
SilentLittleCat <13438082119@163.com>
2018-05-16 16:53:16 +0800

    test

--- routes/friends.php
+++ routes/friends.php
@@ -7,6 +7,7 @@ Route::middleware(['auth.silent:admin'])->group(function () {
     Route::get('/learn/fun/create', 'learn\FunController@create');
     Route::post('/learn/fun/store', 'learn\FunController@store');
     Route::get('/learn/fun/edit', 'learn\FunController@edit');
+    Route::get('/learn/fun/detail', 'learn\FunController@detail');
     Route::post('/learn/fun/update', 'learn\FunController@update');
     Route::post('/learn/fun/delete', 'learn\FunController@delete');
     Route::post('/learn/fun/deleteMany', 'learn\FunController@deleteMany');

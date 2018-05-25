a258ad45c1459f9426c4ec3e937329d1dba2137a
SilentLittleCat <13438082119@163.com>
2018-05-25 16:51:44 +0800

    dream

---
+++ app/HelloFriendsShowRemark.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class HelloFriendsShowRemark extends Model
+{
+    protected $table = 'hello_friends_show_remarks';
+
+    protected $guarded = [];
+}
